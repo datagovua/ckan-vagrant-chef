a75a4d3b1ba5ea2264aa90c084e10ac9e27f6a7e
Brook Elgie <specialbrew@gmail.com>
2015-01-09 14:24:30 +0000

    More attribute refactoring

--- ckan/recipes/ckan_tests.rb
+++ ckan/recipes/ckan_tests.rb
@@ -1,13 +1,10 @@
 # Run tests for ckan and ckanext (optional)
 
 TEST_SQL_DB_NAME = "#{node[:ckan][:sql_db_name]}_test"
-DATASTORE_SQL_USER = "datastore_#{node[:ckan][:project_name]}"
-DATASTORE_SQL_DB_NAME = "datastore_#{node[:ckan][:project_name]}_test"
+TEST_DATASTORE_SQL_DB_NAME = "#{node[:ckan][:datastore][:sql_db_name]}_test"
 ENV['VIRTUAL_ENV'] = "/usr/lib/ckan/#{node[:ckan][:project_name]}"
 SOURCE_DIR = "#{ENV['VIRTUAL_ENV']}/src"
 CKAN_DIR = "#{SOURCE_DIR}/ckan"
-CONFIG_DIR = "/etc/ckan/#{node[:ckan][:project_name]}"
-
 
 # Install dev dependencies
 python_pip "#{CKAN_DIR}/dev-requirements.txt" do
@@ -19,11 +16,11 @@ python_pip "#{CKAN_DIR}/dev-requirements.txt" do
 end
 
 # Create test databases
-postgresql_database "#{SQL_DB_NAME}" do
+postgresql_database "#{TEST_SQL_DB_NAME}" do
   owner node[:ckan][:sql_user]
   encoding "utf8"
 end
-postgresql_database "#{DATASTORE_SQL_DB_NAME}" do
+postgresql_database "#{TEST_DATASTORE_SQL_DB_NAME}" do
   owner node[:ckan][:sql_user]
   encoding "utf8"
 end
@@ -31,17 +28,17 @@ end
 execute "edit test configuration file to setup database url" do
   user node[:ckan][:user]
   cwd CKAN_DIR
-  command "sed -i -e 's/.*sqlalchemy.url.*/sqlalchemy.url=postgresql:\\/\\/#{node[:ckan][:sql_user]}:#{node[:ckan][:sql_password]}@localhost\\/#{SQL_DB_NAME}/' test-core.ini"
+  command "sed -i -e 's/.*sqlalchemy.url.*/sqlalchemy.url=postgresql:\\/\\/#{node[:ckan][:sql_user]}:#{node[:ckan][:sql_password]}@localhost\\/#{TEST_SQL_DB_NAME}/' test-core.ini"
 end
 execute "edit test configuration file to setup write datastore database url" do
   user node[:ckan][:user]
   cwd CKAN_DIR
-  command "sed -i -e 's/.*ckan.datastore.write_url.*/ckan.datastore.write_url=postgresql:\\/\\/#{node[:ckan][:sql_user]}:#{node[:ckan][:sql_password]}@localhost\\/#{DATASTORE_SQL_DB_NAME}/' test-core.ini"
+  command "sed -i -e 's/.*ckan.datastore.write_url.*/ckan.datastore.write_url=postgresql:\\/\\/#{node[:ckan][:sql_user]}:#{node[:ckan][:sql_password]}@localhost\\/#{TEST_DATASTORE_SQL_DB_NAME}/' test-core.ini"
 end
 execute "edit test configuration file to setup read datastore database url" do
   user node[:ckan][:user]
   cwd CKAN_DIR
-  command "sed -i -e 's/.*ckan.datastore.read_url.*/ckan.datastore.read_url=postgresql:\\/\\/#{DATASTORE_SQL_USER}:#{node[:ckan][:sql_password]}@localhost\\/#{DATASTORE_SQL_DB_NAME}/' test-core.ini"
+  command "sed -i -e 's/.*ckan.datastore.read_url.*/ckan.datastore.read_url=postgresql:\\/\\/#{node[:ckan][:datastore][:sql_user]}:#{node[:ckan][:sql_password]}@localhost\\/#{TEST_DATASTORE_SQL_DB_NAME}/' test-core.ini"
 end
 
 # Set permissions on test database tables
